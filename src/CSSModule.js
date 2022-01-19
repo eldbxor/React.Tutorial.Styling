286a9e1278ed03781e68c0e2150c1a2322f3a4f4
yootaek <eldbxor@naver.com>
2022-01-19 17:42:37 +0900

    CSS Module 사용

---
+++ src/CSSModule.js
@@ -0,0 +1,14 @@
+import styles from "./CSSModule.module.css";
+import React from "react";
+
+const CSSModule = () => {
+  console.log(styles);
+
+  return (
+    <div className="styles.wrapper">
+      안녕하세요, 저는 <span className="something">CSS Module!</span>
+    </div>
+  );
+};
+
+export default CSSModule;
