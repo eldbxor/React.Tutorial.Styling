d608b64a324bd5aba62caf97ca59ffc46b0642b8
yootaek <eldbxor@naver.com>
2022-01-21 10:46:08 +0900

    classnames 사용

--- src/CSSModule.js
+++ src/CSSModule.js
@@ -1,11 +1,16 @@
 import styles from "./CSSModule.module.css";
 import React from "react";
+import classNames from "classnames/bind";
+
+const cx = classNames.bind(styles); // 미리 styles에서 클래스를 받아 오도록 설정하고
 
 const CSSModule = () => {
+  const name = "리액트";
+  const message = "제 이름은 ${name}입니다.";
   console.log(styles);
 
   return (
-    <div className="styles.wrapper">
+    <div className={cx("wrapper", "inverted")}>
       안녕하세요, 저는 <span className="something">CSS Module!</span>
     </div>
   );
